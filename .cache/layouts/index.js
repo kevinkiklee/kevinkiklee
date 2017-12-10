3027f42394f3a6816d5d79e2a75fcab4d4b5b9fa
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-10 14:12:57 -0500

    Install Gatsby

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/klee8/Workspace/kevinkiklee.com/src/layouts/index.js"
+  import data from "/Users/klee8/Workspace/kevinkiklee.com/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <img src="http://lorempixel.com/400/200/" alt="" />
+    <p>i have no idea what i'm doing</p>
+  </div>
+)
+
+export default IndexPage
