65fe730add3cc244a6017d6d86fb6926a5d23a34
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-10 14:19:35 -0500

    Initial Build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,15 +3,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/klee8/Workspace/kevinkiklee.com/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/klee8/Workspace/kevinkiklee.com/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/klee8/Workspace/kevinkiklee.com/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/klee8/Workspace/kevinkiklee.com/src/pages/page-2.js"))
 }
 
 exports.json = {
-  "layout-index.json": require("/Users/klee8/Workspace/kevinkiklee.com/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/klee8/Workspace/kevinkiklee.com/.cache/json/dev-404-page.json"),
   "layout-index.json": require("/Users/klee8/Workspace/kevinkiklee.com/.cache/json/layout-index.json"),
   "404.json": require("/Users/klee8/Workspace/kevinkiklee.com/.cache/json/404.json"),
   "layout-index.json": require("/Users/klee8/Workspace/kevinkiklee.com/.cache/json/layout-index.json"),
