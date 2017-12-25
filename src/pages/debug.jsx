92f12110559873da9c78fc39aef0bc84797923ae
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-25 15:00:04 -0500

    Create a Debug page

---
+++ src/pages/debug.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default ({ data }) => {
+  /* eslint-disable no-console */
+  console.log(data)
+  return (
+    <div>
+      <h1>Files</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>relativePath</th>
+            <th>prettySize</th>
+            <th>extension</th>
+            <th>birthTime</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allFile.edges.map(({ node }, index) =>
+            <tr key={index}>
+              <td>
+                {node.relativePath}
+              </td>
+              <td>
+                {node.prettySize}
+              </td>
+              <td>
+                {node.extension}
+              </td>
+              <td>
+                {node.birthTime}
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+/* eslint no-undef: 'off' */
+export const query = graphql`
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
