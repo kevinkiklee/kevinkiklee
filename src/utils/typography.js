f4383df5ae6143f5bb04fb4ba407b36f81bac5d9
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-25 09:20:37 -0500

    Adjust typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,6 +2,28 @@
 import Typography from 'typography'
 import parnassusTheme from 'typography-theme-parnassus'
 
-const typography = new Typography(parnassusTheme)
+const customTheme = {
+  baseFontSize: '18px',
+  googleFonts: [
+    {
+      name: 'Roboto Slab',
+      styles: [
+        '700',
+      ],
+    },
+    {
+      name: 'Merriweather',
+      styles: [
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
+    },
+  ],
+}
+
+const theme = Object.assign({}, parnassusTheme, customTheme)
+const typography = new Typography(theme)
 
 export default typography
