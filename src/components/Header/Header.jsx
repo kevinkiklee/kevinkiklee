fc346f029f5d921623bffa99a8213e3ad71c19c3
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-25 07:24:31 -0500

    Add links to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Link from 'gatsby-link';
 import SiteLogo from '../../assets/logos/weird-bk-64.png'
 import './Header.css'
 
@@ -7,30 +8,38 @@ class Header extends Component {
     return (
       <div className='header-container'>
         <div className='header'>
-          <div className='header__left'>
-            <img
-              className='header__site-logo'
-              src={SiteLogo}
-              alt='Kevin K. Lee site logo'
-            />
-            <p className='header__site-title'>
-              i have no idea<br />
-              what i am doing
-            </p>
-          </div>
+          <Link to='/'>
+            <div className='header__left'>
+              <img
+                className='header__site-logo'
+                src={SiteLogo}
+                alt='Kevin K. Lee site logo'
+              />
+              <p className='header__site-title'>
+                i have no idea<br />
+                what i am doing
+              </p>
+            </div>
+          </Link>
           <div className='header__right'>
-            <p className='header__link'>
-              Data Structures<br />
-              & Algorithms
-            </p>
-            <p className='header__link'>
-              Coding<br />
-              Thoughts
-            </p>
-            <p className='header__link'>
-              Random<br />
-              Ramblings
-            </p>
+            <Link to='/algods'>
+              <p className='header__link'>
+                Algorithms &<br />
+                Data Structures
+              </p>
+            </Link>
+            <Link to='/blog'>
+              <p className='header__link'>
+                Coding<br />
+                Thoughts
+              </p>
+            </Link>
+            <Link to='/'>
+              <p className='header__link'>
+                Random<br />
+                Ramblings
+              </p>
+            </Link>
             <p className='header__link'>
               About<br />
               Kevin
