85330420e3ed9b2483a0603832815948e58afbad
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-26 18:27:51 -0500

    Create Rambles page

--- src/pages/rambles.jsx
+++ src/pages/rambles.jsx
@@ -1,16 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Helmet from 'react-helmet'
+import PostList from '../components/PostList/PostList'
+import SEO from '../components/SEO/SEO'
 import config from '../../data/SiteConfig'
+import './rambles.css'
 
-class RamblesPage extends Component {
+class Rambles extends React.Component {
   render() {
+    const groupEdges = (edges) => (
+      edges.reduce((groupedEdges, edge) => {
+        if (edge.node.frontmatter.sticky) {
+          groupedEdges.sticky.push(edge)
+        } else {
+          groupedEdges.regular.push(edge)
+        }
+
+        return groupedEdges
+      }, {
+        sticky: [],
+        regular: [],
+      })
+    )
+
+    const ramblesEdges = groupEdges(this.props.data.rambles.edges)
+
     return (
-      <div className='code-page-container'>
-        <Helmet title={`Coding Musings | ${config.siteTitle}`} />
-        Coding Musings
+      <div className='rambles-page-container page-container'>
+        <Helmet title={config.siteTitle} />
+        <SEO postEdges={this.props.data.rambles.edges} />
+        <h1>Coding Ramblings</h1>
+        {ramblesEdges.sticky && <PostList postEdges={ramblesEdges.sticky} />}
+        {ramblesEdges.regular && <PostList postEdges={ramblesEdges.regular} />}
       </div>
     )
   }
 }
 
-export default RamblesPage
+export default Rambles
+
+/* eslint no-undef: 'off' */
+export const pageQuery = graphql`
+  query ramblesEdges {
+    rambles: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { category: { eq: "rambles" } } }
+    ) {
+      ...edgeFields
+    }
+  }
+`
