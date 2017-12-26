dabfd136808dc58b1ff4bcd7158e2fee74266bd8
Kevin K. Lee <kevin.kik.lee@gmail.com>
2017-12-26 18:27:33 -0500

    Create AlgoDs Page

--- src/pages/algods.jsx
+++ src/pages/algods.jsx
@@ -1,16 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Helmet from 'react-helmet'
+import PostList from '../components/PostList/PostList'
+import SEO from '../components/SEO/SEO'
 import config from '../../data/SiteConfig'
+import './algods.css'
 
-class AlgoDsPage extends Component {
+class AlgoDs extends React.Component {
   render() {
+    const groupEdges = (edges) => (
+      edges.reduce((groupedEdges, edge) => {
+        if (edge.node.frontmatter.sticky) {
+          groupedEdges.sticky.push(edge)
+        } else {
+          groupedEdges.regular.push(edge)
+        }
+
+        return groupedEdges
+      }, {
+        sticky: [],
+        regular: [],
+      })
+    )
+
+    const algodsEdges = groupEdges(this.props.data.algods.edges)
+
     return (
-      <div className='algods-page-container'>
-        <Helmet title={`About | ${config.siteTitle}`} />
-        Algorithms & Data Structures
+      <div className='algods-page-container page-container'>
+        <Helmet title={config.siteTitle} />
+        <SEO postEdges={this.props.data.algods.edges} />
+        <h1>Algorithms & Data Structures</h1>
+
+        <h3>Introductory Articles</h3>
+        <p>If you are new to this site, please check out these articles:</p>
+        {algodsEdges.sticky && <PostList postEdges={algodsEdges.sticky} />}
+
+        <h3>Algorithms & Data Structures Guides</h3>
+        <p>Learn the fundamentals:</p>
+        {algodsEdges.regular && <PostList postEdges={algodsEdges.regular} />}
       </div>
     )
   }
 }
 
-export default AlgoDsPage
+export default AlgoDs
+
+/* eslint no-undef: 'off' */
+export const pageQuery = graphql`
+  query AlgoDsEdges {
+    algods: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { category: { eq: "algods" } } }
+    ) {
+      ...edgeFields
+    }
+  }
+`
